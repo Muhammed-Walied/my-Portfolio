be9b2250f76512d08b541686a3c5c4904af2db70
Muhammed-Walied <mw2242001@gmail.com>
2026-08-23 23:32:38 +0300

    feat: add centralized social and CV link configuration constants

---
+++ src/constants/links.ts
@@ -0,0 +1,9 @@
+// Centralized links configuration
+// You can put your Google Drive, OneDrive, Dropbox, or Notion CV link here:
+export const CV_LINK = 'https://drive.google.com/file/d/1bhJtQOat6z1tgtJvvCn858VAKeOPyiaf/view?usp=sharing';
+
+export const SOCIAL_LINKS = {
+  github: 'https://github.com/Muhammed-Walied',
+  linkedin: 'https://linkedin.com/in/muhammed-walied-281213277',
+  email: 'mailto:muhammedmahrous48@gmail.com',
+};
